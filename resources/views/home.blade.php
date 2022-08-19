d1ec54e6d859b032af4678542ecce4d42ce94c26
rama <akbar.6b@gmail.com>
2022-08-19 09:35:00 +0700

    add layout component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Careish</title>
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-3xl font-extrabold">
-    Home Page
-  </h1>
-</body>
-</html>
+<x-layout>
+    <h1 class="text-3xl font-extrabold">
+        Home Page
+    </h1>
+</x-layout>
