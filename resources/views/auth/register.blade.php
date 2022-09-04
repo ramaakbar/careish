9a88065eb6eeb88f987bfb3eb211e3a4375f4819
Patrick Thelysander <patrickthelysander@gmail.com>
2022-09-04 23:11:48 +0700

    [upd] - resolved register conflict

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,29 +19,18 @@
                             placeholder="Your name" required>
                     </div>
                     <div class="mb-5">
-<<<<<<< HEAD
                         <label for="email" class="block mb-2.5 text-base font-medium text-gray-900">Email</label>
                         <input id="email" type="text"
-=======
-                        <label for="name" class="block mb-2.5 text-base font-medium text-gray-900">Email</label>
-                        <input id="name" type="text"
->>>>>>> 939de658b9f08de9e2227f4f3c4e3ecfd252a7d3
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Email" required>
                     </div>
                     <div class="mb-5">
-<<<<<<< HEAD
                         <label for="password" class="block mb-2.5 text-base font-medium text-gray-900">Password</label>
                         <input id="password" type="text"
-=======
-                        <label for="name" class="block mb-2.5 text-base font-medium text-gray-900">Password</label>
-                        <input id="name" type="text"
->>>>>>> 939de658b9f08de9e2227f4f3c4e3ecfd252a7d3
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Password" required>
                     </div>
                     <div class="mb-5">
-<<<<<<< HEAD
                         <label for="cpassword" class="block mb-2.5 text-base font-medium text-gray-900">Confirm Password</label>
                         <input id="cpassword" type="text"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
@@ -49,36 +38,18 @@
                     </div>
                     <div class="block mb-[21px]">
                         <button id="createBtn" name="createBtn" class="w-full p-3 text-white transition duration-500 ease-in-out bg-[#4950BA] border-indigo-600 rounded hover:bg-blue-900 text-[16px]">Create Account</button>
-=======
-                        <label for="name" class="block mb-2.5 text-base font-medium text-gray-900">Confirm Password</label>
-                        <input id="name" type="text"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="Confirm Password" required>
-                    </div>
-                    <div class="mb-5 block mb-[21px]">
-                        <button class="w-full p-3 text-white transition duration-500 ease-in-out bg-[#4950BA] border-indigo-600 rounded hover:bg-blue-900 text-[16px]">Create Account</button>
->>>>>>> 939de658b9f08de9e2227f4f3c4e3ecfd252a7d3
                     </div>
                     <div class="mb-5">
                         <p class="text-center text-[#6B7280]">or</p>
                     </div>
-<<<<<<< HEAD
                     <div class="block mb-[29px]">
                         <button type="button" id="signGoogleBtn" name="signGoogleBtn" class="w-full p-3 border-gray-300 border rounded text-[15px] font-medium inline-flex items-center justify-center hover:bg-gray-100 transition duration-500 ease-in-out">
-=======
-                    <div class="mb-5 block mb-[29px]">
-                        <button type="button" class="w-full p-3 border-gray-300 border rounded text-[15px] font-medium inline-flex items-center justify-center hover:bg-gray-100 transition duration-500 ease-in-out">
->>>>>>> 939de658b9f08de9e2227f4f3c4e3ecfd252a7d3
                             <img class="w-6 h-6 mr-2" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" />
                             Sign up with Google
                         </button>
                     </div>
                     <div class="text-center text-[#6B7280]">
-<<<<<<< HEAD
                         Dont have an account yet? <a href="/login" class="text-[#4950BA] font-medium hover:underline transition ease-in-out duration-500">Sign In here</a>
-=======
-                        Dont have an account yet? <a href="" class="text-[#4950BA] font-medium hover:underline transition ease-in-out duration-500"> Sign Up here </a>
->>>>>>> 939de658b9f08de9e2227f4f3c4e3ecfd252a7d3
                     </div>
                 </form>
             </div>
