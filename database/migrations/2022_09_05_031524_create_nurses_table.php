89a004e4b6ab44ae4100e1441fc199f12c6d5610
rama <akbar.6b@gmail.com>
2022-09-05 10:32:05 +0700

    [add] - migration,model,seeder for role,gender,availability and nurse

---
+++ database/migrations/2022_09_05_031524_create_nurses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Availability;
+use App\Models\Gender;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nurses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->foreignIdFor(Gender::class)->constrained();
+            $table->string('address');
+            $table->string('picture');
+            $table->foreignIdFor(Availability::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nurses');
+    }
+};
