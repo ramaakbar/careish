a5ff775eef711402d02a5b390726847d320f2b55
rama <akbar.6b@gmail.com>
2022-09-08 22:43:06 +0700

    [add] - delete user function

--- app/Http/Livewire/Dashboard/UsersDashboardTable.php
+++ app/Http/Livewire/Dashboard/UsersDashboardTable.php
@@ -12,6 +12,18 @@ class UsersDashboardTable extends Component
     use WithPagination;
 
     use TraitsWithSorting;
+
+    public $deleteId = '';
+
+    public function getDeleteId($id) {
+        $this->deleteId = $id;
+    }
+
+    public function delete() {
+        User::destroy($this->deleteId);
+        session()->flash('error', 'User no ' .$this->deleteId. ' has successfully been added');
+        return redirect()->to('/dashboard/users');
+    }
     
     public function render()
     {
