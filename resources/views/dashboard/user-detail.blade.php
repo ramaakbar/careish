ddc0bf2fe7a662dcd5e43b4c6656428e13e61e25
rama <akbar.6b@gmail.com>
2022-09-13 21:06:44 +0700

    [add] - user transaction table on user detail

--- resources/views/dashboard/user-detail.blade.php
+++ resources/views/dashboard/user-detail.blade.php
@@ -6,15 +6,17 @@
                 class="inline text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 Back
             </a>
-            <h1 class="my-4 mb-5 text-3xl font-bold">Edit {{ $user->name }}</h1>
+            <h1 class="my-4 mb-5 text-3xl font-bold">{{ $user->name }} <img
+                    src="{{ $user->picture == 'assets/placeholder_profile.jpeg' ? asset($user->picture) : asset('/storage/' . $user->picture) }}"
+                    alt="picture" class="inline w-8 h-8 mr-2 rounded-full"></h1>
             <div class="flex justify-between mb-5">
                 <div class="flex space-x-4">
                     <div>
-                        <p>Created at</p>
+                        <p class="text-gray-700">Created at</p>
                         <p>{{ Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</p>
                     </div>
                     <div>
-                        <p>Updated at</p>
+                        <p class="text-gray-700">Updated at</p>
                         <p>{{ Carbon\Carbon::parse($user->updated_at)->diffForHumans() }}</p>
                     </div>
                 </div>
@@ -24,7 +26,7 @@
                     Delete
                 </button>
             </div>
-            <div class="w-full p-5 bg-white border rounded ">
+            <div class="w-full p-5 mb-8 bg-white border rounded">
                 <form action="/dashboard/users/{{ $user->id }}" method="POST" enctype="multipart/form-data">
                     @method('put')
                     @csrf
@@ -126,14 +128,20 @@
                             type="submit">
                             Save
                         </button>
-                        <button class="block text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                        <a href="/dashboard/users/{{ $user->id }}"
+                            class="block text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                             type="button">
                             Cancel
-                        </button>
+                        </a>
                     </div>
 
                 </form>
             </div>
+
+            <h2 class="mb-3 text-xl font-bold">Transactions</h2>
+            <livewire:dashboard.user-transaction-dashboard-table :user="$user"/>
+            
+
 
             {{-- confirm modal --}}
             <div id="confirm-modal" tabindex="-1"
