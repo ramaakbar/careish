ed35a55f4719b2db42588c6890d3e66c22cf63e0
rama <akbar.6b@gmail.com>
2022-09-18 22:04:11 +0700

    [del] - remove error

--- resources/views/livewire/dashboard/add-nurse-modal.blade.php
+++ resources/views/livewire/dashboard/add-nurse-modal.blade.php
@@ -68,10 +68,7 @@
                                 <label for="city_id" class="block mb-2 text-sm font-medium text-gray-900 ">City</label>
                                 <x-select wire:model="city_id" placeholder="Select city" :async-data="route('cities', ['provinces_id' => $province_id])" option-label="name" option-value="id"
                                     value="{{ $city_id }}" />
-                                @error('city_id')
-                                    <p class="mt-2 text-sm text-red-600"><span class="font-medium">Oh,
-                                            snapp!</span> {{ $message }}</p>
-                                @enderror
+
                             </div>
                         </div>      
                         
