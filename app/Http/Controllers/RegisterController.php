4e86487d59d7430799a93e13eca93a933bde63c5
rama <akbar.6b@gmail.com>
2022-09-20 20:17:54 +0700

    [add] - register function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class RegisterController extends Controller
-{
+class RegisterController extends Controller {
     public function index() {
         return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|min:2|max:50',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:200|confirmed',
+        ]);
+
+        $validated['phone_number'] = '';
+        $validated['role_id'] = 1;
+        $validated['password'] = Hash::make($validated['password']);
+        $validated['picture'] = 'assets/placeholder_profile.jpeg';
+
+        User::create($validated);
+        return redirect('/login')->with('success', 'Account successfully created! you can now login');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,54 +2,78 @@
     <div class="flex flex-row">
         <div class="w-full lg:w-8/12">
             <div class="flex flex-col justify-center max-w-xl px-4 pt-8 mx-auto md:pt-0 md:h-screen">
-              <div>
-                <a href="/" class="block mb-4 text-3xl font-extrabold text-gray-900">
-                    Careish.
-                </a>
-                <h2 class="text-2xl font-bold text-[gray-900] block mb-1">
-                    Sign Up
-                </h2>
-                <p class="mb-6 text-[#6B7280]">Create an account to get started</p>
-              </div>
-                <form action="">
+                <div>
+                    <a href="/" class="block mb-4 text-3xl font-extrabold text-gray-900">
+                        Careish.
+                    </a>
+                    <h2 class="text-2xl font-bold text-[gray-900] block mb-1">
+                        Sign Up
+                    </h2>
+                    <p class="mb-6 text-[#6B7280]">Create an account to get started</p>
+                </div>
+                <form action="/register" method="POST">
+                    @csrf
                     <div class="mb-5">
                         <label for="name" class="block mb-2.5 text-base font-medium text-gray-900">Name</label>
-                        <input id="name" type="text"
+                        <input id="name" name="name" type="text"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="Your name" required>
+                            placeholder="Your name" value="{{ old('name') }}" required >
+                        @error('name')
+                            <p class="mt-2 text-sm text-red-600"><span class="font-medium">Oh,
+                                    snapp!</span> {{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="mb-5">
                         <label for="email" class="block mb-2.5 text-base font-medium text-gray-900">Email</label>
-                        <input id="email" type="text"
+                        <input id="email" name="email" type="text"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="Email" required>
+                            placeholder="Email" value="{{ old('email') }}" required>
+                        @error('email')
+                            <p class="mt-2 text-sm text-red-600"><span class="font-medium">Oh,
+                                    snapp!</span> {{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="mb-5">
                         <label for="password" class="block mb-2.5 text-base font-medium text-gray-900">Password</label>
-                        <input id="password" type="text"
+                        <input id="password" name="password" type="password"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Password" required>
+                        @error('password')
+                            <p class="mt-2 text-sm text-red-600"><span class="font-medium">Oh,
+                                    snapp!</span> {{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="mb-5">
-                        <label for="cpassword" class="block mb-2.5 text-base font-medium text-gray-900">Confirm Password</label>
-                        <input id="cpassword" type="text"
+                        <label for="password_confirmation"
+                            class="block mb-2.5 text-base font-medium text-gray-900">Confirm Password</label>
+                        <input id="password_confirmation" name="password_confirmation" type="password"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             placeholder="Confirm Password" required>
+                        @error('password_confirmation')
+                            <p class="mt-2 text-sm text-red-600"><span class="font-medium">Oh,
+                                    snapp!</span> {{ $message }}</p>
+                        @enderror
                     </div>
                     <div class="block mb-[21px]">
-                        <button id="createBtn" name="createBtn" class="w-full p-3 text-white transition duration-500 ease-in-out bg-[#4950BA] border-indigo-600 rounded hover:bg-blue-900 text-[16px]">Create Account</button>
+                        <button type="submit" id="createBtn"
+                            class="w-full p-3 text-white transition duration-500 ease-in-out bg-[#4950BA] border-indigo-600 rounded hover:bg-blue-900 text-[16px]">Create
+                            Account</button>
                     </div>
                     <div class="mb-5">
                         <p class="text-center text-[#6B7280]">or</p>
                     </div>
                     <div class="block mb-[29px]">
-                        <button type="button" id="signGoogleBtn" name="signGoogleBtn" class="w-full p-3 border-gray-300 border rounded text-[15px] font-medium inline-flex items-center justify-center hover:bg-gray-100 transition duration-500 ease-in-out">
-                            <img class="w-6 h-6 mr-2" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" />
+                        <button id="signGoogleBtn"
+                            class="w-full p-3 border-gray-300 border rounded text-[15px] font-medium inline-flex items-center justify-center hover:bg-gray-100 transition duration-500 ease-in-out">
+                            <img class="w-6 h-6 mr-2"
+                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" />
                             Sign up with Google
                         </button>
                     </div>
                     <div class="text-center text-[#6B7280]">
-                        Dont have an account yet? <a href="/login" class="text-[#4950BA] font-medium hover:underline transition ease-in-out duration-500">Sign In here</a>
+                        Dont have an account yet? <a href="/login"
+                            class="text-[#4950BA] font-medium hover:underline transition ease-in-out duration-500">Sign
+                            In here</a>
                     </div>
                 </form>
             </div>
