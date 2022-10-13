b190493ef02fe5050b5ce91abc1b214abd15f15e
rama <akbar.6b@gmail.com>
2022-10-13 21:44:18 +0700

    [add] - user profile setting page

---
+++ app/Http/Livewire/User/ChangePasswordModal.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class ChangePasswordModal extends Component {
+
+    public $password;
+
+    public $password_confirmation;
+
+    public function clear() {
+        $this->resetErrorBag();
+        $this->reset(['password', 'password_confirmation']);
+    }
+
+    protected $rules = [
+        'password' => 'required|min:8|max:200|confirmed',
+    ];
+
+    public function submit() {
+        $this->validate();
+
+        $hashPassword = Hash::make($this->password);
+
+        User::where('id', Auth::id())->update([
+            'password' => $hashPassword,
+        ]);
+
+        $this->clear();
+        Alert::toast('Successfully change password', 'success');
+        return redirect()->to('/user/profile-setting');
+    }
+
+    public function render() {
+        return view('livewire.user.change-password-modal');
+    }
+}
