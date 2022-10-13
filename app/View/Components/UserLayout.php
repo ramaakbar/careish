1fcc861e7b132522d66e211154cef2f91b5a2bfe
rama <akbar.6b@gmail.com>
2022-10-13 22:05:26 +0700

    [add] - active link

--- app/View/Components/UserLayout.php
+++ app/View/Components/UserLayout.php
@@ -21,10 +21,10 @@ class UserLayout extends Component {
      */
     public function render() {
         $sidebar = [
-            'Transaction List' => ['/user/transaction-list', 'heroicon-o-shopping-bag'],
-            'Profile Setting' => ['/user/profile-setting', 'heroicon-o-user'],
-            'Favorites' => ['/user/favorites', 'heroicon-o-bookmark'],
-            'Reviews' => ['/user/reviews', 'heroicon-o-star']
+            'Transaction List' => ['/user/transaction-list', 'heroicon-o-shopping-bag', 'user/transaction-list'],
+            'Profile Setting' => ['/user/profile-setting', 'heroicon-o-user', 'user/profile-setting'],
+            'Favorites' => ['/user/favorites', 'heroicon-o-bookmark', 'user/favorites'],
+            'Reviews' => ['/user/reviews', 'heroicon-o-star', 'user/reviews']
         ];
         return view('components.user-layout', compact('sidebar'));
     }
--- resources/views/components/user-layout.blade.php
+++ resources/views/components/user-layout.blade.php
@@ -4,12 +4,13 @@
             <img class="mr-3 rounded-full w-14 h-14" src="/assets/placeholder_man.jpeg" alt="user photo">
             <h3 class="text-base font-medium">Akbar Ramadhan Yusri</h3>
         </div>
-        <nav class="flex flex-col">
+        <nav class="flex flex-col space-y-2">
             @foreach ($sidebar as $name => $test)
                 <a href={{ $test[0] }}
-                    class="flex items-center px-2 py-2 text-gray-600 rounded hover:bg-gray-100 ">{{ svg($test[1], 'w-6 h-6') }}
+                    class="flex items-center px-2 py-2 text-gray-600 rounded hover:bg-gray-100 {{ request()->is($test[2]) ? 'bg-gray-100' : '' }}">{{ svg($test[1], 'w-6 h-6') }}
                     <span class="ml-3">{{ $name }}</span></a>
             @endforeach
+
         </nav>
     </aside>
     <section class="flex flex-col w-full lg:w-3/4">
