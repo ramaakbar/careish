185d6b2e7df32956f3b3b931b54a9c47d18c1804
rama <akbar.6b@gmail.com>
2022-11-14 22:48:13 +0700

    [upd] - create post with images

--- app/Http/Livewire/Component/Trix.php
+++ app/Http/Livewire/Component/Trix.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Livewire\Component;
 
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Trix extends Component {
+    use WithFileUploads;
 
     const EVENT_VALUE_UPDATED = 'trix_value_updated';
     public $value;
     public $trixId;
+    public $photos = [];
 
     public function mount($value = '') {
         $this->value = $value;
@@ -17,6 +21,20 @@ class Trix extends Component {
 
     public function updatedValue($value) {
         $this->emit(self::EVENT_VALUE_UPDATED, $this->value);
+    }
+
+    public function completeUpload(string $uploadedUrl, string $trixUploadCompletedEvent) {
+
+        foreach ($this->photos as $photo) {
+            if ($photo->getFilename() == $uploadedUrl) {
+                $newFilename = $photo->store('post-images');
+                $url = Storage::url($newFilename);
+                $this->dispatchBrowserEvent($trixUploadCompletedEvent, [
+                    'url' => $url,
+                    'href' => $url,
+                ]);
+            }
+        }
     }
 
     public function render() {
--- resources/views/livewire/component/trix.blade.php
+++ resources/views/livewire/component/trix.blade.php
@@ -5,9 +5,42 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"></script>
     <script>
         var trixEditor = document.getElementById("{{ $trixId }}")
+        var mimeTypes = ["image/png", "image/jpeg", "image/jpg"];
 
         addEventListener("trix-blur", function(event) {
             @this.set('value', trixEditor.getAttribute('value'))
-        })
+        });
+
+        addEventListener("trix-file-accept", function(event) {
+            if (!mimeTypes.includes(event.file.type)) {
+                return event.preventDefault();
+            }
+        });
+
+        addEventListener("trix-attachment-add", function(event) {
+            uploadTrixImage(event.attachment);
+        });
+
+        function uploadTrixImage(attachment) {
+            @this.upload(
+                'photos',
+                attachment.file,
+                function(uploadedURL) {
+                    const trixUploadCompletedEvent = `trix-upload-completed:${btoa(uploadedURL)}`;
+                    const trixUploadCompletedListener = function(event) {
+                        attachment.setAttributes(event.detail);
+                        window.removeEventListener(trixUploadCompletedEvent, trixUploadCompletedListener);
+                    }
+
+                    window.addEventListener(trixUploadCompletedEvent, trixUploadCompletedListener);
+
+                    @this.call('completeUpload', uploadedURL, trixUploadCompletedEvent);
+                },
+                function() {},
+                function(event) {
+                    attachment.setUploadProgress(event.detail.progress);
+                },
+            )
+        }
     </script>
 </div>
