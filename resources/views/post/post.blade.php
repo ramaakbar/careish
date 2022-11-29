9c2b4e56eacafe31bd60ff68565c32fad6ceb3af
Patrick Thelysander <patrickthelysander@gmail.com>
2022-11-29 22:51:22 +0700

    [upd] - removal of dark classes

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -12,17 +12,15 @@
                 <h2 class="mt-4 mb-6 text-xl font-bold xs:text-2xl sm:text-3xl">Comments</h2>
                 <form action="/articles/{{ $post->id }}/comment" method="POST">
                     @csrf
-                    <div
-                        class="w-full mt-6 mb-10 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                        <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                    <div class="w-full mt-6 mb-10 border border-gray-200 rounded-lg bg-gray-50 ">
+                        <div class="px-4 py-2 bg-white rounded-t-lg">
                             <label for="comment" class="sr-only">Your comment</label>
                             <textarea id="comment" rows="4" name="comment"
-                                class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-                                placeholder="Write a comment..." required></textarea>
+                                class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-0 " placeholder="Write a comment..." required></textarea>
                         </div>
-                        <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+                        <div class="flex items-center justify-between px-3 py-2 border-t ">
                             <button type="submit"
-                                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-[#504BE5] rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-[#504BE5] rounded-lg focus:ring-4 focus:ring-blue-200  hover:bg-blue-800">
                                 Post comment
                             </button>
                         </div>
