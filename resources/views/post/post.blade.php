661f527105d73c51eaeb6e62fd7f2afe724ee501
Patrick Thelysander <patrickthelysander@gmail.com>
2022-11-29 21:40:27 +0700

    [upd] - margin bottom of comment input

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -13,7 +13,7 @@
                 <form action="/articles/{{ $post->id }}/comment" method="POST">
                     @csrf
                     <div
-                        class="w-full mt-6 mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                        class="w-full mt-6 mb-10 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                         <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
                             <label for="comment" class="sr-only">Your comment</label>
                             <textarea id="comment" rows="4" name="comment"
