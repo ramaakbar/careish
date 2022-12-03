b83e2b376be889cba3f596287947bff9ba295d03
rama <akbar.6b@gmail.com>
2022-12-03 22:27:10 +0700

    [upd] - remove dark class

--- resources/views/components/chat-button-floating.blade.php
+++ resources/views/components/chat-button-floating.blade.php
@@ -10,7 +10,7 @@
         <span class="sr-only">Icon description</span>
     </a>
     <div id="tooltip-default" role="tooltip"
-        class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+        class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip">
         Chat with admin
         <div class="tooltip-arrow" data-popper-arrow></div>
     </div>
