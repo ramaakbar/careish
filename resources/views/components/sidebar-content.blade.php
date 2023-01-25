4fe2d897a003d76cde1cb8946eaff0bd7b171d7d
rama <akbar.6b@gmail.com>
2023-01-25 23:06:35 +0700

    fix name sidebar

--- resources/views/components/sidebar-content.blade.php
+++ resources/views/components/sidebar-content.blade.php
@@ -105,7 +105,7 @@
                         aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                         <img class="w-8 h-8 mr-2 rounded-full" src="{{ asset('/storage/' . Auth::user()->picture) }}"
                             alt="user photo">
-                        <span class="flex-1 ml-3 text-left whitespace-nowrap">Jamal</span>
+                        <span class="flex-1 ml-3 text-left whitespace-nowrap">{{ Auth::user()->name }}</span>
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
