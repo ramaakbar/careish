c2e9876ba32d41226d0cae3d397794ec8707537b
rama <akbar.6b@gmail.com>
2024-05-21 23:05:46 +0700

    add ig endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\api\Cities;
 use App\Http\Controllers\api\Provinces;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Instagram\Api;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/provinces', Provinces::class)->name('provinces');
 
-Route::get('/cities/{provinces_id?}', [Cities::class,'getCities'])->name('cities');
+Route::get('/cities/{provinces_id?}', [Cities::class, 'getCities'])->name('cities');
+
+Route::get('/ig/{username}', function (string $username) {
+    $cachePool = new FilesystemAdapter('Instagram', 0, __DIR__ . '/../cache');
+    $api = new Api($cachePool);
+    $api->login(env('INSTAGRAM_EMAIL'), env('INSTAGRAM_PASSWORD'));
+    $profile = $api->getProfile($username);
+    return $api->getMoreMedias($profile)->toArray();
+});
